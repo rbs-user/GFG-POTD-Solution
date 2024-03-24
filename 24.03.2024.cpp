dce506e7786618cd60bd07ef28e1ba9374b2eb05
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-03-24 14:01:31 +0530

    Update 24.03.2024.cpp

--- 24.03.2024.cpp
+++ 24.03.2024.cpp
@@ -1,1 +1,19 @@
 // 24.03.2024 gfg potd solution
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x){
+        stack<int>aux;
+        while(!st.empty()){
+            aux.push(st.top());
+            st.pop();
+        }
+        
+        st.push(x);
+        
+        while(!aux.empty()){
+            st.push(aux.top());
+            aux.pop();
+        }
+        return st;
+    }
+};
