a277c7ddfa7014fff4ccdd15492a1992c7c9c206
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-03-25 19:45:32 +0530

    Update 25.03.2024.cpp

--- 25.03.2024.cpp
+++ 25.03.2024.cpp
@@ -1,1 +1,28 @@
 // 25.03.2024 gfg potd solution
+class Solution{
+public:	
+    void solve(vector<string>&ans, string curr, int ones, int zeroes, int n){
+        if(ones<zeroes) return;
+        
+        if(curr.size()==n){
+            ans.push_back(curr);
+            return;
+        }
+        
+        solve(ans, curr+'1', ones+1, zeroes, n);
+        solve(ans, curr+'0', ones, zeroes+1, n);
+        
+        return;
+    }
+
+	vector<string> NBitBinary(int n)
+	{
+	    // Your code goes here
+	    vector<string>ans;
+	    string curr="";
+	    
+	    solve(ans, curr, 0, 0, n);
+	    
+	    return ans;
+	}
+};
