8124741be77e4f78a84171df118bf8030a3c584e
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-03-30 11:22:03 +0530

    Update 30.03.2024.cpp

--- 30.03.2024.cpp
+++ 30.03.2024.cpp
@@ -1,1 +1,16 @@
 // 30.03.2024 gfg potd solution
+// Function to find the minimum element in the given BST.
+
+class Solution {
+public:   
+    int minValue(Node* root) {
+        // Code here
+        if(!root) return -1;
+        
+        while(root->left != NULL){
+            root=root->left;
+        }
+        
+        return root->data;
+    }
+};
