e67d4d2721b99fa212adee73bfd262ab77d8bbf3
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-03-31 11:47:45 +0530

    Update 31.03.2024.cpp

--- 31.03.2024.cpp
+++ 31.03.2024.cpp
@@ -1,1 +1,29 @@
 // 31.03.2024 gfg potd solution
+class Solution {
+  public:
+    int ans=INT_MIN;
+    
+    void solve(Node* root, int n){
+        if(root==NULL) return;
+        
+        if(root->key <= n){
+            ans=max(ans, root->key);
+            solve(root->right, n);
+        }
+        else{
+            solve(root->left, n);
+        }
+        
+        return;
+    }
+    int findMaxForN(Node* root, int n) {
+        // code here
+        solve(root, n);
+        if(ans==INT_MIN){
+            ans=-1;
+        }
+        
+        return ans;
+        
+    }
+};
