0c00f64ded7a5a71f83b4428373e760da24f9e7f
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-04 17:54:37 +0530

    Update 04.04.2024.cpp

--- 04.04.2024.cpp
+++ 04.04.2024.cpp
@@ -1,1 +1,22 @@
 // 04.04.2024 gfg potd solution
+class Solution
+{
+    public:
+    //Function to find sum of all possible substrings of the given string.
+    long long sumSubstrings(string s){
+        
+        // your code here
+        long  long prev=0, curr=0, ans=0;
+        long long mod=1e9+7;
+        
+        for(int i=0;i<s.length();i++){
+            curr=(s[i]-'0')*(i+1) + prev*10;
+            curr%=mod;
+            ans+=curr;
+            ans%=mod;
+            prev=curr;
+        }
+        
+        return ans;
+    }
+};
