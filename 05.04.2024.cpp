8078768031ac29a8544b49db19b18b9b3d12be53
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-05 16:57:07 +0530

    Update 05.04.2024.cpp

--- 05.04.2024.cpp
+++ 05.04.2024.cpp
@@ -1,1 +1,27 @@
 // 05.04.2024 gfg potd solution
+class Solution {
+  public:
+    int LIS(vector<int>&arr){
+        int n=arr.size();
+        vector<int>dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j] && arr[i]-arr[j]>=i-j){
+                    dp[i]=max(dp[i], 1+dp[j]);
+                }
+            }
+        }
+        int maxi=0;
+        for(auto it:dp){
+            maxi=max(maxi, it);
+        }
+        return maxi;
+    }
+
+    int min_operations(vector<int>& nums) {
+        // Code here
+        int l=LIS(nums);
+        int n=nums.size();
+        return n-l;
+    }
+};
