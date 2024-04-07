2c669da6bd4b128de539b04e777e12f7b0cd2f17
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-07 13:02:10 +0530

    Update 07.04.2024.cpp

--- 07.04.2024.cpp
+++ 07.04.2024.cpp
@@ -1,1 +1,18 @@
 // 07.04.2024 gfg potd solution
+class Solution{
+		
+	public:
+	int maxDotProduct(int n, int m, int a[], int b[]) 
+	{ 
+		// Your code goes here
+		int dp[m+1][n+1];
+		memset(dp, 0, sizeof(dp));
+		
+		for(int i=1;i<=m;i++){
+		    for(int j=i;j<=n;j++){
+		        dp[i][j]=max(dp[i-1][j-1]+a[j-1]*b[i-1], dp[i][j-1]);
+		    }
+		}
+		return dp[m][n];
+	} 
+};
