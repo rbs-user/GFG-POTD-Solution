09022160014aa7c14f3ddf1d1ee044dbae3c0af9
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-10 12:41:30 +0530

    Update 10.04.2024.cpp

--- 10.04.2024.cpp
+++ 10.04.2024.cpp
@@ -1,1 +1,12 @@
 // 10.04.2024 gfg potd solution
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        // code here
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=ans^(arr[i]);
+        }
+        return ans;
+    }
+};
