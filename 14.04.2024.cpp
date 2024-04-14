0b804d11aa03d2f958db261aef60697e8b083e08
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-14 21:30:55 +0530

    Update 14.04.2024.cpp

--- 14.04.2024.cpp
+++ 14.04.2024.cpp
@@ -1,1 +1,25 @@
 // 14.04.2024 gfg potd solution
+class Solution {
+  public:
+    void printArr(int n, int arr[]) {
+        // Your code for printing array here
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    void setToZero(int n, int arr[]) {
+        // Use memset to set arr to zero
+        for(int i=0;i<n;i++){
+            arr[i]=0;
+        }
+    }
+
+    void xor1ToN(int n, int arr[]) {
+        // Xor arr[i]^i
+        for(int i=0;i<n;i++){
+            arr[i]=arr[i]^i;
+        }
+    }
+};
