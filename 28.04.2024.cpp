ecfa8c0c9b187336ba0bf634096c62523d54d589
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-28 16:36:40 +0530

    Create 28.04.2024.cpp

---
+++ 28.04.2024.cpp
@@ -0,0 +1,17 @@
+// 28.04.2024 gfg potd solution
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        // Your Code Here
+        Node* fast=head, *slow=head, *prev=NULL;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            prev=slow;
+            slow=slow->next;
+        }
+        
+        prev->next=slow->next;
+        return head;
+    }
+};
