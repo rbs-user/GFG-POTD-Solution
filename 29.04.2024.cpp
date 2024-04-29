a6ee7e477f32b5172a5f57274191737983de672c
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-04-29 17:07:08 +0530

    Update 29.04.2024.cpp

--- 29.04.2024.cpp
+++ 29.04.2024.cpp
@@ -1,1 +1,19 @@
 // 29.04.2024 gfg potd solution
+class Solution {
+    public:
+    Node* deleteK(Node *head,int K){
+    if(K == 1) return nullptr;
+    Node* s = head;
+    int cnt = 0;
+    for(Node* s = head; s != nullptr; s = s->next) {
+        cnt++;
+        if(cnt == K-1){
+            cnt = 0;
+            if(s->next)
+                s->next = s->next->next;
+        }
+
+    }
+    return head;
+}
+};
