3381bdad6c04fa6170feb2323d9da6666d08c485
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-05-07 14:43:14 +0530

    Update 07.05.2024.cpp

--- 07.05.2024.cpp
+++ 07.05.2024.cpp
@@ -1,1 +1,27 @@
 // 07.05.2024 gfg potd solution
+vector<int> reverseLevelOrder(Node *root)
+{
+    // code here
+    vector<int>ans;
+    stack<int>st;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        Node* node=q.front(); q.pop();
+        st.push(node->data);
+        
+        if(node->right){
+            q.push(node->right);
+        }
+        if(node->left){
+            q.push(node->left);
+        }
+    }
+    
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    
+    return ans;
+}
