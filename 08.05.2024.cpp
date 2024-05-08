83570f1842fb276756d1efaf5140d2b0f06a06da
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-05-08 20:11:03 +0530

    Update 08.05.2024.cpp

--- 08.05.2024.cpp
+++ 08.05.2024.cpp
@@ -1,1 +1,30 @@
-08.05.2024 gfg potd solution
+// 08.05.2024 gfg potd solution
+class Solution {
+  public:
+    void solve(Node* root, vector<int>curr, vector<vector<int>>&ans){
+        if(root==NULL){
+            return;
+        }
+        
+        curr.push_back(root->data);
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(curr);
+            return ;
+        }
+        
+        solve(root->left, curr, ans);
+        solve(root->right, curr, ans);
+        
+        return;
+    }
+  
+    vector<vector<int>> Paths(Node* root) {
+        // code here
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(root, curr, ans);
+        
+        
+        return ans;
+    }
+};
