81861fb6fc92e8d109ec966c0cfcb3010ecf3f63
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-05-17 22:47:28 +0530

    Create 17.05.2024.cpp

---
+++ 17.05.2024.cpp
@@ -0,0 +1,24 @@
+// 17.05.2024 gfg potd solution
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int i=0, j=1;
+        while(j<n){
+            int diff=abs(arr[j]-arr[i]);
+            
+            if(diff==x){
+                return 1;
+            }
+            else if(diff<x){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        return -1;
+    }
+};
