707d8c049718798559d295fc6a9b2c46d5aa67ad
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-05-20 22:34:31 +0530

    Update 20.05.2024.cpp

--- 20.05.2024.cpp
+++ 20.05.2024.cpp
@@ -1,1 +1,20 @@
 // 20.05.2024 gfg potd solution
+class Solution
+{
+	public:
+		long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    // Code here
+		    if(n==0) return 1;
+		    if(n==1) return x;
+		    
+		    long long val= PowMod(x, n/2, M);
+		    val=(val*val)%M;
+		    
+		    if(n%2==1){
+		        return (val*x)%M;
+		    }
+		    
+		    return val;
+		}
+};
