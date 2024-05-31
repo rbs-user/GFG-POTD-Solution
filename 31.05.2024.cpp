c4864b5a80ec37dcf3f87c5b60a8a8d75a70f6be
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-05-31 20:51:02 +0530

    Create 31.05.2024.cpp

---
+++ 31.05.2024.cpp
@@ -0,0 +1,14 @@
+// 31.05.2024 gfg potd solution 
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+        int rn=n &  (0b11110000);
+        int ln=n & (0b00001111);
+        
+        rn=rn>>4;
+        ln=ln<<4;
+        
+        return rn | ln;
+    }
+};
