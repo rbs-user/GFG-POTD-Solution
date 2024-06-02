1f8084ea85d5b6d6d8a0f9c329622f5df5a15ff7
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-06-02 18:22:45 +0530

    Create 02.06.2024.cpp

---
+++ 02.06.2024.cpp
@@ -0,0 +1,21 @@
+// 02.06.2024 gfg potd solution
+class Solution {
+  public:
+    vector<int> constructList(int q, vector<vector<int>> &arr) {
+        // code here
+        vector<int>ans;
+        int x=0;
+        
+        for(int i=arr.size()-1;i>=0;i--){
+            if(arr[i][0]==0){
+                ans.push_back(x^arr[i][1]);
+            }
+            else{
+                x^=arr[i][1];
+            }
+        }
+        ans.push_back(0^x);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
