add6fa7e1c95ebc84694d7240ccf840a14b8af2a
Roshan Sharma <114395817+God-Rex@users.noreply.github.com>
2024-06-08 20:56:38 +0530

    Create 08.06.2024.cpp

---
+++ 08.06.2024.cpp
@@ -0,0 +1,13 @@
+// 08.06.2024 gfg potd solution
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        for(int i=0;i<n-1;i++){
+            if(arr1[i]!=arr2[i]){
+                return i;
+            }
+        }
+        return n-1;
+    }
+};
